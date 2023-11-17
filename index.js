bd52fdd26f501fcb06d5ee89134d940fa5edb476
MatveySuperStar <sergeev2000matvey@mail.ru>
2023-11-17 22:44:28 +0300

    update server3

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Expose-Headers",
+    "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Origin", process.env.HOST_CLIENT);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json());
 app.use("/api/email", emailRouter);
 
